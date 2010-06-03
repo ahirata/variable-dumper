4d617a3a4a4bbb63a98ffd223db0726ce278d403
ahirata <ahirata@myhost.localdomain>
2010-06-03 00:29:31 -0300

    improving async job execution and timeout parametrization

--- src/atarih/variabledumper/ui/actions/VariableDumperAction.java
+++ src/atarih/variabledumper/ui/actions/VariableDumperAction.java
@@ -17,7 +17,17 @@ import atarih.variabledumper.handler.TypeHandler;
 
 public class VariableDumperAction implements IViewActionDelegate {
 
-    private static final int TIMEOUT = 500;
+    // defaults to 500
+    private static int TIMEOUT = 500;
+
+    static {
+        String timeoutProperty = System.getProperty("variableDumperTimeout");
+        if (timeoutProperty != null && !timeoutProperty.isEmpty()) {
+            try {
+                TIMEOUT = Integer.parseInt(timeoutProperty);
+            } catch (Exception e) { }
+        }
+    };
 
     private static final class VariableDumpJob extends Job {
         private final JDIVariable variable;
@@ -34,18 +44,23 @@ public class VariableDumperAction implements IViewActionDelegate {
                 TypeHandler typeHandler = new TypeHandler();
                 typeHandler.setMonitor(monitor);
                 typeHandler.handleVariable("", variable);
-
                 status = Status.OK_STATUS;
             } catch (DebugException e) {
                 e.printStackTrace();
                 status = e.getStatus();
+            } catch (Exception e) {
+                e.printStackTrace();
+                status = new Status(IStatus.ERROR, "variable-dumper", e.getMessage());
+            }
+            synchronized(this) {
+                notifyAll();
             }
             return status;
         }
     }
 
     @Override
-    public void init(IViewPart view) { }
+    public void init(IViewPart view) {  }
 
     @Override
     @SuppressWarnings("restriction")
@@ -63,7 +78,10 @@ public class VariableDumperAction implements IViewActionDelegate {
             job.schedule();
 
             try {
-                Thread.sleep(TIMEOUT);
+                synchronized (job) {
+                    job.wait(TIMEOUT);
+                }
+
             } catch (InterruptedException e) {
                 System.out.println("exception while sleeping");
                 e.printStackTrace();
